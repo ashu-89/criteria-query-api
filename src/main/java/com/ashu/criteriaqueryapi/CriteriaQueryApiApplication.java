e8fc5f12e206d32b64f44208125992c090695660
Ashutosh Pathak <ashu.p89@gmail.com>
2023-08-20 19:09:01 +0530

    initial commit

---
+++ src/main/java/com/ashu/criteriaqueryapi/CriteriaQueryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ashu.criteriaqueryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriteriaQueryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriteriaQueryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashu/criteriaqueryapi/CriteriaQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.criteriaqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriaQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
